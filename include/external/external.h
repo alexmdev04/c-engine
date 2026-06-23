9c262c8746e1da5ccbde2408de13a64a5f18d981
Alex M <alexmdev04@gmail.com>
2026-06-23 17:34:36 +0100

    Add .gitkeep for build dir

--- include/external/external.h
+++ include/external/external.h
@@ -3,13 +3,15 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wstatic-in-inline"
 #include "SDL3/SDL.h"
+#include "cglm/cglm.h"
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "cglm/cglm.h"
+
 #pragma clang diagnostic pop
 
 #endif
