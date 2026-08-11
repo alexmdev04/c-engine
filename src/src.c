a404f7490392b78c6223f3aa5668b016b19c68b0
Alex M <alexmdev04@gmail.com>
2026-08-11 18:16:36 +0100

    Add comments to src

--- src/src.c
+++ src/src.c
@@ -1,10 +1,22 @@
 #ifndef SRC
 #define SRC
+
+// External headers
+
 #include "external.h"
+
+// Globals
+
 #include "types.c"
 #include "globals.c"
+
+// Components
+
 #include "time.c"
 #include "vk.c"
 #include "app.c"
+
+// Main
+
 #include "main.c"
 #endif
