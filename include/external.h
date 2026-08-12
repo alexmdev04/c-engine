5479aab4f62c68903e8c4a764c62898587398874
Alex M <alexmdev04@gmail.com>
2026-08-12 04:34:03 +0100

    Add volk

--- include/external.h
+++ include/external.h
@@ -1,14 +1,20 @@
 #pragma once
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#pragma clang diagnostic ignored "-Wnullability-extension"
 #pragma clang diagnostic ignored "-Wstatic-in-inline"
-#include "SDL3/SDL.h"
-#include "cglm/cglm.h"
-#include "vulkan/vulkan.h"
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_vulkan.h"
+#include "cglm/cglm.h"
+#define VOLK_IMPLEMENTATION
+#include "volk/volk.h"
 #include "vma/vk_mem_alloc.h"
+// #include "vulkan/vulkan.h"
 #pragma clang diagnostic pop
